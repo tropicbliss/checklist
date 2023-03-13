d86ee244974d10122a34df72e806371e905fbf0a
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-03-13 13:31:45 +0800

    E

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   return (
     <main>
